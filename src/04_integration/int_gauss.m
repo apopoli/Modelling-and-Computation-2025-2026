9726f94f07846c3d1efc7f1085a120a7305934de
Arturo Popoli <arturo.popoli@unibo.it>
2025-11-18 09:55:54 +0100

    sync

--- src/04_integration/int_gauss.m
+++ src/04_integration/int_gauss.m
@@ -1,16 +1,24 @@
-function [int] = int_gauss(f,a,b)
+function [int] = int_gauss(f,a,b,n)
 %int_gauss Computes a definite integral with gauss quadrature
 %   f: handle to function to be integrated
 %   a,b: boundaries of the integration interval
+%   n: number of gauss points to be used
 
-% assume: n = 1 % 1 gauss point
-% w = 2 (weight)
-% tau = 0
+switch n
+    case 1
+        w = 2;
+        tau = 0;
+    case 2
+        w = [1 1];
+        tau = [-1/sqrt(3) 1/sqrt(3)];
+    case 3
+        w = [5/9 8/9 5/9];
+        tau = [-sqrt(3/5) 0 sqrt(3/5)];
+end
 
-w = 2;
 m = (b-a)/2;
 q = (a+b)/2;
 
-int = w * m * f(m*tau + q);
+int = sum(w .* m .* f(m.*tau + q));
 
 end
